71de1bb659421f18531ad1cae456aa8e9458279a
Rafal Korytkowski <rafal@openmrs.org>
2012-06-01 23:03:09 +0200

    Corrected imports.

--- omod/src/main/java/org/openmrs/module/privilegehelper/web/controller/PrivilegeAssignerController.java
+++ omod/src/main/java/org/openmrs/module/privilegehelper/web/controller/PrivilegeAssignerController.java
@@ -14,7 +14,6 @@
 package org.openmrs.module.privilegehelper.web.controller;
 
 import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
